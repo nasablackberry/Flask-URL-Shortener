0f8a2069af4d590837c73726e93e5dbe24e9f283
Shivank Singh <72298367+nasablackberry@users.noreply.github.com>
2024-08-08 12:37:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+import string
+from flask import Flask, render_template, redirect, url_for, request 
+
+app = Flask(__name__)
+short_urls = {}
+
+def generate_short_url(length=6):
+    chars=string.ascii_letters+string.digits
+    short_url = "".join(random.choice(chars) for _ in range(length))
+    return short_url
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method=="POST":
+        long_url=request.form['long_url']
+        short_url = generate_short_url()
+        while short_url in short_urls:
+            short_url = generate_short_url()
+        short_urls[short_url] = long_url
+        return f"Shortened URL: {request.url_root}{short_url}"
+    return render_template("index.html")
+
+@app.route("/<short_url>")
+def redirect_url(short_url):
+    long_url = short_urls.get(short_url)
+    if long_url:
+        return redirect(long_url)
+    else:
+        return "URL Not Found", 404
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
